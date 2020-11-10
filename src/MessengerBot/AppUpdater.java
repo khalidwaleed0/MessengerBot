243518f9b7810bce44216941c3d735a311baf210
khalidwaleed0 <khalidwaleed0@outlook.com>
2020-11-10 15:19:33 +0200

    Update AppUpdater.java

--- src/MessengerBot/AppUpdater.java
+++ src/MessengerBot/AppUpdater.java
@@ -29,7 +29,7 @@ public class AppUpdater {
 	{
 		Document doc = Jsoup.connect("https://github.com/khalidwaleed0/MessengerBot/releases").get();
 		Element latestReleaseName = doc.selectFirst(".f1.flex-auto.min-width-0.text-normal a");
-		if(!latestReleaseName.text().equals("v1.1.0"))
+		if(!latestReleaseName.text().equals("v2.0"))
 		{
 			showUpdateWindow();
 			String whatsNew = doc.selectFirst(".markdown-body p").wholeText();
